8d6b4f039f8c1384d9a4ce3b2d6fec9841bfd683
rong1002 <borong1002@gmail.com>
2020-10-24 20:56:12 +0800

    Initial Commit

---
+++ Ten Year Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Ten Year Challenge
+//
+//  Created by Lin Bo Rong on 2020/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Ten Year Challenge/Ten_Year_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ten_Year_ChallengeApp.swift
+//  Ten Year Challenge
+//
+//  Created by Lin Bo Rong on 2020/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Ten_Year_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
